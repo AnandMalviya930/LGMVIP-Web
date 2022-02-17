ca7b58ea4cdcae970811719bfe4a23a1dee1bbaf
AnandMalviya930 <95901670+AnandMalviya930@users.noreply.github.com>
2022-02-17 17:27:52 +0530

    Add files via upload

---
+++ Task-2/src/App.js
@@ -0,0 +1,42 @@
+import Users from "./includes/grid";
+import "./App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users_data: [], loading: false };
+    this.updateState = this.updateState.bind(this);
+  }
+  updateState() {
+    this.setState({ loading: true });
+
+    setTimeout(
+      async function () {
+        const response = await fetch("https://reqres.in/api/users?page=1");
+        const jsonresponse = await response.json();
+        console.log(jsonresponse);
+        this.setState({ users_data: jsonresponse["data"], loading: false });
+      }.bind(this),
+      2000
+    );
+  }
+  render() {
+    return (
+      <>
+        <nav className="nav">
+          <div className="items">
+            <h2>Grow-More Fashion</h2>
+            <button className="fetchbtn" onClick={this.updateState}>
+              Show users
+            </button>
+          </div>
+        </nav>
+        <div className="userdatacontainer">
+          <Users loading={this.state.loading} users={this.state.users_data} />
+        </div>
+      </>
+    );
+  }
+}
+export default App;
